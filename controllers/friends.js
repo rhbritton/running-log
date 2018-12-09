3ad2e554859c508af204389e20d9bd6767c97aa6
Ryan Britton <ryanhbritton@gmail.com>
2018-12-09 17:08:46 -0500

    debugging friend request

--- controllers/friends.js
+++ controllers/friends.js
@@ -86,24 +86,24 @@ Controller.requestFriend = function(req, res, next) {
 		if (err) 
 			return res.json({ err: 'Database Error' });
 
-		// if (!potentialFriend) 
-		// 	return res.json({ err: req.body.email+' does not exist' });
+		if (!potentialFriend) 
+			return res.json({ err: req.body.email+' does not exist' });
 
-		// var potentialFriendFriends = [];
-		// potentialFriend.friends && potentialFriend.friends.forEach(function(request) {
-		// 	potentialFriendFriends.push(request.toString());
-		// });
+		var potentialFriendFriends = [];
+		potentialFriend.friends && potentialFriend.friends.forEach(function(request) {
+			potentialFriendFriends.push(request.toString());
+		});
 
-		// if (potentialFriendFriends.includes(req.session.user._id.toString()))
-		// 	return res.json({ err: 'You are already friends with '+potentialFriend.email });
+		if (potentialFriendFriends.includes(req.session.user._id.toString()))
+			return res.json({ err: 'You are already friends with '+potentialFriend.email });
 
-		// var potentialFriendRequestsReceived = [];
-		// potentialFriend.requestsReceived && potentialFriend.requestsReceived.forEach(function(request) {
-		// 	potentialFriendRequestsReceived.push(request.toString());
-		// });
+		var potentialFriendRequestsReceived = [];
+		potentialFriend.requestsReceived && potentialFriend.requestsReceived.forEach(function(request) {
+			potentialFriendRequestsReceived.push(request.toString());
+		});
 
-		// if (potentialFriendRequestsReceived.includes(req.session.user._id.toString())) 
-		// 	return res.json({ err: potentialFriend.email+' has already received your friend request' });
+		if (potentialFriendRequestsReceived.includes(req.session.user._id.toString())) 
+			return res.json({ err: potentialFriend.email+' has already received your friend request' });
 
 		// User.findOne({ _id: req.session.user._id }, function(err, user) {
 		// 	if (err) 
